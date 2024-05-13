31f63fd19ec45817976aa4f8e4daef369128baf1
Brunogabriel12 <169068345+Brunogabriel12@users.noreply.github.com>
2024-05-13 16:51:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("|--------------------------------|");
+    System.out.println("| *        Sobrenatural         *|");
+    System.out.println("|________________________________|");
+
+      System.out.println("");
+
+    System.out.println("Sobrenatural é uma série de televisão que segue as aventuras de dois irmãos, Sam e Dean Winchester, enquanto caçam criaturas sobrenaturais, como demônios, fantasmas, vampiros e muito mais. A série começa com os irmãos reunindo-se após a misteriosa morte de sua mãe, que foi causada por uma força sobrenatural. Guiados pelo pai deles, os irmãos se tornam caçadores, viajando pelos Estados Unidos em um Chevy Impala, investigando e combatendo ameaças sobrenaturais enquanto lidam com seus próprios demônios internos e segredos de família. Ao longo das temporadas, a série mistura elementos de horror, suspense, drama familiar e mitologia, construindo um universo rico e cativante que conquistou uma base de fãs devota.");
+
+      System.out.println("");
+
+      System.out.println("|------------------------------|");
+      System.out.println("|*           Trailer          *|");
+      System.out.println("|______________________________|");
+
+      System.out.println("");
+    
+      System.out.println("Sobrenatural - Trailer Oficial");
+      System.out.println("https://youtu.be/apltEQy8RzQ?si=AJCibTLoMQpIwKmE");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
